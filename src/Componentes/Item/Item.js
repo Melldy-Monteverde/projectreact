6c0aa6dbed16123d4fa0674c273859576acbff88
Melldy Monteverde <monteverdelex@gmail.com>
2022-07-23 22:12:46 -0300

    desafio contador y map

--- src/Componentes/Item/Item.js
+++ src/Componentes/Item/Item.js
@@ -4,6 +4,8 @@ const Item = ({product}) => {
         <li className="li-products">
             <h3>{product.name}</h3>
             <img src={product.img} alt={product.name}/>
+            <button className="verDetalle">Ver detalle{product.description}</button>
+            <p>Stock: {product.stock}</p>
         </li>
     )
 }
